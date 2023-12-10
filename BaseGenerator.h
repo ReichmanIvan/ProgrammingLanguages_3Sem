5fa3d9688c3d366fdb871f7396be1ff26e8ff40d
Райхман Иван ТКИ-141 <113427606+ReichmanIvan@users.noreply.github.com>
2023-12-11 00:56:51 +0300

    Create BaseGenerator.h

---
+++ BaseGenerator.h
@@ -0,0 +1,18 @@
+#pragma once
+
+/**
+* @brief базовый класс Generator.
+*/
+class Generator
+{
+public:
+	/**
+	* @brief Виртуальный деструктор.
+	*/
+	virtual ~Generator() = 0 {};
+
+	/**
+	* @brief Виртуальный метод генератор.
+	*/
+	virtual int generate() = 0;
+};
